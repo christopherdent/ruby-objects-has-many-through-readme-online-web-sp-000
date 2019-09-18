ed5703094058209a19b25667b65f19a8ff6377f7
Christopher Dent <christopherdent01@gmail.com>
2019-09-18 00:25:32 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,9 +2,12 @@ class Waiter
   attr_accessor :name, :experience
   
   
+  @@all = []
+  
   def initialize(name, years)
     @name = name
     @experience = experience 
+    @@all < self 
     
     
   end 
