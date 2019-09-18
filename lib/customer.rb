427c631ccf5f4e3eb7e6874ef48211a1e1ef62e5
Christopher Dent <christopherdent01@gmail.com>
2019-09-18 00:23:30 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,20 @@
 class Customer
+  attr_accessor :name, :age
+  
+  @@customers = []
   
   
-  def initialize
+  
+  def initialize(name, age)
+    @name = name
+    @age = age 
+    @@customers << self 
     
   end 
   
+  def self.all 
+    @@customers 
+  end 
   
   
   
